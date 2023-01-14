38e671064a6950f42d3fc58f65a1e7cb4232aa8a
Olle Thunberg <olle@fls.se>
2023-01-14 23:49:36 +0100

    played around with websockets

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/ThunbergOlle/mc-voip
 
 go 1.19
 
-require github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc // indirect
+require (
+	github.com/gofrs/uuid v4.0.0+incompatible // indirect
+	github.com/gomodule/redigo v1.8.4 // indirect
+	github.com/gordonklaus/portaudio v0.0.0-20221027163845-7c3b689db3cc // indirect
+	github.com/gorilla/websocket v1.5.0
+	golang.org/x/net v0.5.0 // indirect
+
+)
--- main.go
+++ main.go
@@ -2,16 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/url"
 	"os"
 	"os/signal"
 
 	"github.com/ThunbergOlle/mc-voip/internal/microphone"
 	"github.com/ThunbergOlle/mc-voip/pkg/errorHandler"
 	"github.com/gordonklaus/portaudio"
+	"github.com/gorilla/websocket"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+	u := url.URL{Scheme: "ws", Host: "localhost:3000", Path: "/socket.io/?EIO=4&transport=websocket"}
+
+	connection, resp, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Printf("handshake failed with status %d", resp.StatusCode)
+		log.Fatal("dial:", err)
+	}
+	//When the program closes close the connection
+	defer connection.Close()
+	// send socketio connect message
+	err = connection.WriteMessage(websocket.TextMessage, []byte("40"))
+	if err != nil {
+		log.Fatal("write:", err)
+	}
+
+	log.Printf("NewClient success\n")
 
 	killSig := make(chan os.Signal, 1)
 	signal.Notify(killSig, os.Interrupt, os.Kill)
@@ -35,10 +54,12 @@ func main() {
 	errorHandler.Panic(stream.Start())
 	for {
 		err := stream.Write()
+
 		if err != nil {
 			fmt.Println("Error handling speaker stream: " + err.Error())
 			panic(err)
 		}
+
 		select {
 		case <-killSig:
 			fmt.Println("Kill signal received")
